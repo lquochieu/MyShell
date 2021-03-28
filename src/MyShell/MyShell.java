ff6a6e668fecdc002412c2bb9e09ad0223d4132b
OOLT.VN.20202.20190051.TranTrongHiep <trantronghiep2001@gmail.com>
2021-03-28 08:55:22 +0700

    MyShell

--- src/MyShell/MyShell.java
+++ src/MyShell/MyShell.java
@@ -1,23 +1,25 @@
 package MyShell;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MyShell {
 	static final String EXIT_COMMAND = "exit";
 	static String currentWorkingDirectory;
 	static CurrentDir dir = new CurrentDir();
-	public static void main(String[] args) {
-		System.out.println(Messenger.WELCOME);
-		
-		Scanner read = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		System.out.println(Messenger.WELCOME.getMessenger());
+		BufferedReader console = new BufferedReader (new InputStreamReader(System.in));
 		currentWorkingDirectory = dir.getDir();
 		JobManager jobs = new JobManager();
 		
 		while(true) {
-			System.out.println(Messenger.NEW_COMMAND);
-			String commandLine = read.nextLine().trim();
+			System.out.print(Messenger.NEW_COMMAND.getMessenger());
+			String commandLine = console.readLine().trim();
 			if(commandLine.equals(EXIT_COMMAND)) {
-				System.out.println(Messenger.GOODBYE);
+				System.out.println(Messenger.GOODBYE.getMessenger());
 				break;
 			}
 			else {
